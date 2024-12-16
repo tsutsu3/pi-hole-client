72c587edd7eb589a1ad2a6850a7b2435535a5069
tsutsu3 <tsutsu3prog@gmail.com>
2024-12-16 22:08:02 +0900

    Add privacy policy link and app detail modal in settings

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -5,3 +5,6 @@ const issueUrl = 'https://github.com/tsutsu3/pi-hole-client/issues';
 
 const supportUrl =
     'https://docs.google.com/forms/d/e/1FAIpQLSdNm7H2iDUaQ0q-JA6hvrUSsGe50_iL5NVK6fR_2hkCsyrA-A/viewform?hl=en';
+
+const privacyPolicyUrl =
+    'https://github.com/tsutsu3/pi-hole-client/wiki/Privacy-policy';
