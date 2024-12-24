82c079b6403aebb6f152d9ab9674b4ea1582ae30
tsutsu3 <tsutsu3prog@gmail.com>
2024-12-24 18:37:15 +0900

    Fix message

--- lib/gateways/v5/api_gateway_v5.dart
+++ lib/gateways/v5/api_gateway_v5.dart
@@ -480,7 +480,7 @@ class ApiGatewayV5 implements ApiGateway {
         } else {
           return SetWhiteBlacklistResponse(
             result: APiResponseType.error,
-            message: 'Invalid response format',
+            message: 'not_exists',
           );
         }
       } else {
@@ -634,7 +634,7 @@ class ApiGatewayV5 implements ApiGateway {
         } else {
           return RemoveDomainFromListResponse(
             result: APiResponseType.error,
-            message: 'Invalid response format',
+            message: 'not_exists',
           );
         }
       } else {
