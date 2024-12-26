1e34db35e65c6d33ec230f55d7ca00f4e4f0c3e1
tsutsu3 <tsutsu3prog@gmail.com>
2024-12-26 23:51:34 +0900

    Fix biometrics

--- android/app/src/main/kotlin/io/github/tsutsu3/pi_hole_client/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/tsutsu3/pi_hole_client/MainActivity.kt
@@ -1,5 +1,5 @@
 package io.github.tsutsu3.pi_hole_client
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
