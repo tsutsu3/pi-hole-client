79ca5549d7552fc76abdb348fdecb027b8086380
tsutsu3 <tsutsu3prog@gmail.com>
2024-12-29 16:10:55 +0900

    Add tests for FiltersProvider v6

--- test/ut/providers/filters_provider_test.dart
+++ test/ut/providers/filters_provider_test.dart
@@ -34,6 +34,14 @@ void main() {
         filtersProvider.statusAllowedAndRetried,
         FiltersV5().statusAllowedAndRetried,
       );
+      expect(
+        filtersProvider.defaultSelected,
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+      );
+      expect(
+        filtersProvider.statusSelectedString,
+        'Blocked (gravity)',
+      );
     });
 
     test('updates to FiltersV6 when server version is v6', () {
@@ -89,6 +97,203 @@ void main() {
         expect(
           filtersProvider.statusSelected,
           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        );
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'resetTime calls resetTime on _filters and notifies listeners',
+      () {
+        final dateTime = DateTime.now();
+        filtersProvider.setStartTime(dateTime);
+        filtersProvider.setEndTime(dateTime);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetTime();
+        expect(filtersProvider.startTime, null);
+        expect(filtersProvider.endTime, null);
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'resetStatus calls resetStatus on _filters and notifies listeners',
+      () {
+        filtersProvider.setStatusSelected([1, 2, 3]);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetStatus();
+        expect(
+          filtersProvider.statusSelected,
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        );
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'setClients updates clients and notifies listeners',
+      () {
+        void listener() {}
+        filtersProvider.addListener(listener);
+        final clients = ['client1', 'client2'];
+        filtersProvider.setClients(clients);
+        expect(filtersProvider.totalClients, clients);
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'setSelectedClients updates selectedClients and notifies listeners',
+      () {
+        void listener() {}
+        filtersProvider.addListener(listener);
+        final selectedClients = ['client1'];
+        filtersProvider.setSelectedClients(selectedClients);
+        expect(filtersProvider.selectedClients, selectedClients);
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test('setSelectedDomain updates selectedDomain and notifies listeners', () {
+      void listener() {}
+      filtersProvider.addListener(listener);
+      final domain = 'example.com';
+      filtersProvider.setSelectedDomain(domain);
+      expect(filtersProvider.selectedDomain, domain);
+      filtersProvider.removeListener(listener);
+    });
+
+    test(
+      'resetClients calls resetClients on _filters and notifies listeners',
+      () {
+        final selectedClients = ['client1'];
+        filtersProvider.setSelectedClients(selectedClients);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetClients();
+        expect(filtersProvider.totalClients, []);
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test('setRequestStatus updates requestStatus and notifies listeners', () {
+      void listener() {}
+      filtersProvider.addListener(listener);
+      final status = RequestStatus.allowed;
+      filtersProvider.setRequestStatus(status);
+
+      expect(filtersProvider.requestStatus, status);
+      filtersProvider.removeListener(listener);
+    });
+  });
+
+  group('FiltersProvider v6', () {
+    late FiltersProvider filtersProvider;
+    late MockServersProvider mockServersProvider;
+
+    setUp(() {
+      mockServersProvider = MockServersProvider();
+      when(mockServersProvider.selectedServer).thenReturn(
+        Server(
+          address: 'http://localhost:8081',
+          alias: 'test v6',
+          defaultServer: false,
+          apiVersion: 'v6',
+        ),
+      );
+
+      filtersProvider = FiltersProvider(serversProvider: mockServersProvider);
+    });
+
+    test('initializes with FiltersV6 by default', () {
+      expect(
+        filtersProvider.statusAllowedAndRetried,
+        FiltersV6().statusAllowedAndRetried,
+      );
+      expect(
+        filtersProvider.defaultSelected,
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+      );
+      expect(
+        filtersProvider.statusSelectedString,
+        'Blocked (gravity)',
+      );
+    });
+
+    test('setStatusSelected updates statusSelected and notifies listeners', () {
+      void listener() {}
+      filtersProvider.addListener(listener);
+      filtersProvider.setStatusSelected([1, 2, 3]);
+      expect(filtersProvider.statusSelected, [1, 2, 3]);
+      filtersProvider.removeListener(listener);
+    });
+
+    test('setStartTime updates startTime and notifies listeners', () {
+      void listener() {}
+      filtersProvider.addListener(listener);
+      final dateTime = DateTime.now();
+      filtersProvider.setStartTime(dateTime);
+      expect(filtersProvider.startTime, dateTime);
+      filtersProvider.removeListener(listener);
+    });
+
+    test('setEndTime updates endTime and notifies listeners', () {
+      void listener() {}
+      filtersProvider.addListener(listener);
+      final dateTime = DateTime.now();
+      filtersProvider.setEndTime(dateTime);
+      expect(filtersProvider.endTime, dateTime);
+      filtersProvider.removeListener(listener);
+    });
+
+    test(
+      'resetFilters calls resetFilters on _filters and notifies listeners',
+      () {
+        filtersProvider.setStatusSelected([1, 2, 3]);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetFilters();
+        expect(
+          filtersProvider.statusSelected,
+          [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        );
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'resetTime calls resetTime on _filters and notifies listeners',
+      () {
+        final dateTime = DateTime.now();
+        filtersProvider.setStartTime(dateTime);
+        filtersProvider.setEndTime(dateTime);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetTime();
+        expect(filtersProvider.startTime, null);
+        expect(filtersProvider.endTime, null);
+        filtersProvider.removeListener(listener);
+      },
+    );
+
+    test(
+      'resetStatus calls resetStatus on _filters and notifies listeners',
+      () {
+        filtersProvider.setStatusSelected([1, 2, 3]);
+
+        void listener() {}
+        filtersProvider.addListener(listener);
+        filtersProvider.resetStatus();
+        expect(
+          filtersProvider.statusSelected,
+          [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
         );
         filtersProvider.removeListener(listener);
       },
