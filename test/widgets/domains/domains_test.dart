33daeb1d7fe6e14c48223cd8c59180b51efeaddd
tsutsu3 <tsutsu3prog@gmail.com>
2025-01-06 00:48:22 +0900

    Remove domainlistprovder mock

--- test/widgets/domains/domains_test.dart
+++ test/widgets/domains/domains_test.dart
@@ -20,7 +20,7 @@ import 'package:pi_hole_client/config/globals.dart';
 import './domains_test.mocks.dart';
 
 @GenerateMocks(
-  [AppConfigProvider, DomainsListProvider, ServersProvider, ApiGatewayV6],
+  [AppConfigProvider, ServersProvider, ApiGatewayV6],
 )
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -37,14 +37,59 @@ void main() async {
     late MockApiGatewayV6 mockApiGatewayV6;
     late MockAppConfigProvider mockConfigProvider;
     late MockServersProvider mockServersProvider;
-    late MockDomainsListProvider mockDomainsListProvider;
+    late DomainsListProvider domainsListProvider;
 
     setUp(() async {
       mockApiGatewayV6 = MockApiGatewayV6();
       mockConfigProvider = MockAppConfigProvider();
       mockServersProvider = MockServersProvider();
-      mockDomainsListProvider = MockDomainsListProvider();
 
+      when(mockServersProvider.selectedServer).thenReturn(server);
+      when(mockServersProvider.selectedApiGateway).thenReturn(mockApiGatewayV6);
+
+      when(mockApiGatewayV6.getDomainLists()).thenAnswer((_) async {
+        return GetDomainLists(
+          result: APiResponseType.success,
+          data: DomainListResult(
+            blacklist: [
+              Domain(
+                id: 3,
+                type: 1,
+                domain: 'black01.example.com',
+                enabled: 1,
+                dateAdded: DateTime.now(),
+                dateModified: DateTime.now(),
+                comment: null,
+                groups: [0],
+              ),
+            ],
+            whitelist: [
+              Domain(
+                id: 1,
+                type: 1,
+                domain: 'white01.example.com',
+                enabled: 1,
+                dateAdded: DateTime.now(),
+                dateModified: DateTime.now(),
+                comment: null,
+                groups: [0],
+              ),
+              Domain(
+                id: 2,
+                type: 1,
+                domain: 'white02.example.com',
+                enabled: 1,
+                dateAdded: DateTime.now(),
+                dateModified: DateTime.now(),
+                comment: null,
+                groups: [0],
+              ),
+            ],
+            whitelistRegex: [],
+            blacklistRegex: [],
+          ),
+        );
+      });
       when(mockApiGatewayV6.removeDomainFromList(any)).thenAnswer((_) async {
         return RemoveDomainFromListResponse(result: APiResponseType.success);
       });
@@ -53,61 +98,8 @@ void main() async {
       when(mockConfigProvider.showingSnackbar).thenReturn(showingSnackbar);
       when(mockConfigProvider.setShowingSnackbar(any)).thenAnswer((_) {});
 
-      when(mockServersProvider.selectedServer).thenReturn(server);
-      when(mockServersProvider.selectedApiGateway).thenReturn(mockApiGatewayV6);
-
-      when(mockDomainsListProvider.fetchDomainsList(any))
-          .thenAnswer((_) async {});
-      when(mockDomainsListProvider.setSelectedTab(any)).thenReturn(null);
-      when(mockDomainsListProvider.searchMode).thenReturn(false);
-      when(mockDomainsListProvider.searchTerm).thenReturn('');
-      when(mockDomainsListProvider.setSearchMode(any)).thenReturn(null);
-      when(mockDomainsListProvider.onSearch(any)).thenReturn(null);
-      when(mockDomainsListProvider.setBlacklistDomains(any)).thenReturn(null);
-      when(mockDomainsListProvider.removeDomainFromList(any)).thenReturn(null);
-      when(mockDomainsListProvider.blacklistDomains).thenReturn(
-        [
-          Domain(
-            id: 1,
-            type: 1,
-            domain: 'black.example.com',
-            enabled: 1,
-            dateAdded: DateTime.now(),
-            dateModified: DateTime.now(),
-            comment: null,
-            groups: [0],
-          ),
-        ],
-      );
-      when(mockDomainsListProvider.whitelistDomains).thenReturn(
-        [
-          Domain(
-            id: 1,
-            type: 1,
-            domain: 'white.example.com',
-            enabled: 1,
-            dateAdded: DateTime.now(),
-            dateModified: DateTime.now(),
-            comment: null,
-            groups: [0],
-          ),
-        ],
-      );
-      when(mockDomainsListProvider.loadingStatus).thenReturn(LoadStatus.loaded);
-      when(mockDomainsListProvider.filteredWhitelistDomains).thenReturn(
-        [
-          Domain(
-            id: 1,
-            type: 1,
-            domain: 'white.example.com',
-            enabled: 1,
-            dateAdded: DateTime.now(),
-            dateModified: DateTime.now(),
-            comment: null,
-            groups: [0],
-          ),
-        ],
-      );
+      domainsListProvider =
+          DomainsListProvider(serversProvider: mockServersProvider);
     });
 
     testWidgets(
@@ -128,7 +120,7 @@ void main() async {
                 create: (context) => mockServersProvider,
               ),
               ChangeNotifierProxyProvider<ServersProvider, DomainsListProvider>(
-                create: (context) => mockDomainsListProvider,
+                create: (context) => domainsListProvider,
                 update: (context, serverConfig, servers) =>
                     servers!..update(serverConfig),
               ),
@@ -156,20 +148,19 @@ void main() async {
 
         // Show whiltelist domains screen
         expect(find.byType(DomainLists), findsOneWidget);
+        await tester.pumpAndSettle();
+        expect(find.text('white01.example.com'), findsOneWidget);
+        expect(find.text('white02.example.com'), findsOneWidget);
 
         // Tap whitelist domain to open domain detail screen
-        await tester.tap(find.text('white.example.com'));
+        await tester.tap(find.text('white01.example.com'));
         await tester.pumpAndSettle();
-
-        // Show domain detail screen
         expect(find.byType(DomainDetailsScreen), findsOneWidget);
         expect(find.byIcon(Icons.delete_rounded), findsOneWidget);
 
         // Tap delete button
         await tester.tap(find.byIcon(Icons.delete_rounded));
         await tester.pumpAndSettle();
-
-        // Show delete domain modal
         expect(
           find.text('Are you sure you want to delete this domain?'),
           findsOneWidget,
@@ -179,8 +170,6 @@ void main() async {
         // Tap confirm button
         await tester.tap(find.text('Confirm'));
         await tester.pump(const Duration(milliseconds: 1000));
-
-        // show snackbar
         expect(find.text('Domain removed successfully'), findsWidgets);
 
         // Show whiltelist domains screen
@@ -194,46 +183,10 @@ void main() async {
         tester.view.physicalSize = const Size(1000, 400);
         tester.view.devicePixelRatio = 1.0;
 
-        when(mockApiGatewayV6.getDomainLists()).thenAnswer((_) async {
-          return GetDomainLists(
-            result: APiResponseType.success,
-            data: DomainListResult(
-              blacklist: [],
-              whitelist: [
-                Domain(
-                  id: 1,
-                  type: 1,
-                  domain: 'white01.example.com',
-                  enabled: 1,
-                  dateAdded: DateTime.now(),
-                  dateModified: DateTime.now(),
-                  comment: null,
-                  groups: [0],
-                ),
-                Domain(
-                  id: 2,
-                  type: 1,
-                  domain: 'white02.example.com',
-                  enabled: 1,
-                  dateAdded: DateTime.now(),
-                  dateModified: DateTime.now(),
-                  comment: null,
-                  groups: [0],
-                ),
-              ],
-              whitelistRegex: [],
-              blacklistRegex: [],
-            ),
-          );
-        });
-
         addTearDown(() {
           tester.view.resetPhysicalSize();
           tester.view.resetDevicePixelRatio();
         });
-
-        final domainsListProvider =
-            DomainsListProvider(serversProvider: mockServersProvider);
 
         await tester.pumpWidget(
           MultiProvider(
