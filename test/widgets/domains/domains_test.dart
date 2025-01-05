f8c8ba3b0b22fa16a667b2302b3b5b131e7c6079
tsutsu3 <tsutsu3prog@gmail.com>
2025-01-06 01:01:18 +0900

    Add domains tests

--- test/widgets/domains/domains_test.dart
+++ test/widgets/domains/domains_test.dart
@@ -93,6 +93,9 @@ void main() async {
       when(mockApiGatewayV6.removeDomainFromList(any)).thenAnswer((_) async {
         return RemoveDomainFromListResponse(result: APiResponseType.success);
       });
+      when(mockApiGatewayV6.addDomainToList(any)).thenAnswer((_) async {
+        return AddDomainToListResponse(result: APiResponseType.success);
+      });
 
       bool showingSnackbar = false;
       when(mockConfigProvider.showingSnackbar).thenReturn(showingSnackbar);
@@ -106,6 +109,81 @@ void main() async {
       'should delete a domain from whitelist and show confirmation modal',
       (WidgetTester tester) async {
         tester.view.physicalSize = const Size(1000, 400);
+        tester.view.devicePixelRatio = 1.0;
+
+        addTearDown(() {
+          tester.view.resetPhysicalSize();
+          tester.view.resetDevicePixelRatio();
+        });
+
+        await tester.pumpWidget(
+          MultiProvider(
+            providers: [
+              ChangeNotifierProvider<ServersProvider>(
+                create: (context) => mockServersProvider,
+              ),
+              ChangeNotifierProxyProvider<ServersProvider, DomainsListProvider>(
+                create: (context) => domainsListProvider,
+                update: (context, serverConfig, servers) =>
+                    servers!..update(serverConfig),
+              ),
+              ChangeNotifierProvider<AppConfigProvider>(
+                create: (context) => mockConfigProvider,
+              ),
+              ChangeNotifierProxyProvider<AppConfigProvider, ServersProvider>(
+                create: (context) => mockServersProvider,
+                update: (context, appConfig, servers) =>
+                    servers!..update(appConfig),
+              ),
+            ],
+            child: MaterialApp(
+              home: const Scaffold(
+                body: DomainLists(),
+              ),
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                AppLocalizations.delegate,
+              ],
+              scaffoldMessengerKey: scaffoldMessengerKey,
+            ),
+          ),
+        );
+
+        // Show whiltelist domains screen
+        expect(find.byType(DomainLists), findsOneWidget);
+        await tester.pumpAndSettle();
+        expect(find.text('white01.example.com'), findsOneWidget);
+        expect(find.text('white02.example.com'), findsOneWidget);
+
+        // Tap whitelist domain to open domain detail screen
+        await tester.tap(find.text('white01.example.com'));
+        await tester.pumpAndSettle();
+        expect(find.byType(DomainDetailsScreen), findsOneWidget);
+        expect(find.byIcon(Icons.delete_rounded), findsOneWidget);
+
+        // Tap delete button
+        await tester.tap(find.byIcon(Icons.delete_rounded));
+        await tester.pumpAndSettle();
+        expect(
+          find.text('Are you sure you want to delete this domain?'),
+          findsOneWidget,
+        );
+        expect(find.text('Confirm'), findsOneWidget);
+
+        // Tap confirm button
+        await tester.tap(find.text('Confirm'));
+        await tester.pump(const Duration(milliseconds: 1000));
+        expect(find.text('Domain removed successfully'), findsWidgets);
+
+        // Show whiltelist domains screen
+        expect(find.byType(DomainLists), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'should delete a domain with mobile screen size',
+      (WidgetTester tester) async {
+        tester.view.physicalSize = const Size(600, 1000);
         tester.view.devicePixelRatio = 1.0;
 
         addTearDown(() {
@@ -248,6 +326,70 @@ void main() async {
         expect(find.text('Domains'), findsOneWidget);
         expect(find.text('white01.example.com'), findsOneWidget);
         expect(find.text('white02.example.com'), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'should add a domain to whitelist',
+      (WidgetTester tester) async {
+        tester.view.physicalSize = const Size(1000, 400);
+        tester.view.devicePixelRatio = 1.0;
+
+        addTearDown(() {
+          tester.view.resetPhysicalSize();
+          tester.view.resetDevicePixelRatio();
+        });
+
+        await tester.pumpWidget(
+          MultiProvider(
+            providers: [
+              ChangeNotifierProvider<ServersProvider>(
+                create: (context) => mockServersProvider,
+              ),
+              ChangeNotifierProxyProvider<ServersProvider, DomainsListProvider>(
+                create: (context) => domainsListProvider,
+                update: (context, serverConfig, servers) =>
+                    servers!..update(serverConfig),
+              ),
+              ChangeNotifierProvider<AppConfigProvider>(
+                create: (context) => mockConfigProvider,
+              ),
+              ChangeNotifierProxyProvider<AppConfigProvider, ServersProvider>(
+                create: (context) => mockServersProvider,
+                update: (context, appConfig, servers) =>
+                    servers!..update(appConfig),
+              ),
+            ],
+            child: MaterialApp(
+              home: const Scaffold(
+                body: DomainLists(),
+              ),
+              localizationsDelegates: [
+                GlobalMaterialLocalizations.delegate,
+                AppLocalizations.delegate,
+              ],
+              scaffoldMessengerKey: scaffoldMessengerKey,
+            ),
+          ),
+        );
+
+        // Show whiltelist domains screen
+        expect(find.byType(DomainLists), findsOneWidget);
+        await tester.pumpAndSettle();
+        expect(find.text('Domains'), findsOneWidget);
+
+        // Tap add button
+        await tester.tap(find.byIcon(Icons.add));
+        await tester.pumpAndSettle();
+        expect(find.text('Add domain'), findsOneWidget);
+        await tester.enterText(find.byType(TextField), 'white03.example.com');
+        await tester.pumpAndSettle();
+        await tester.tap(find.text('Add'));
+        await tester.pumpAndSettle();
+
+        // Show whiltelist domains screen
+        expect(find.text('Domain added successfully'), findsWidgets);
+        expect(find.byType(DomainLists), findsOneWidget);
       },
     );
   });
