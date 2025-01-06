57c2c62e301f6d3508a3db85967f408e54d229c7
tsutsu3 <tsutsu3prog@gmail.com>
2025-01-07 06:57:54 +0900

    refactor: merge convs.dart into conversions.dart

--- lib/functions/conversions.dart
+++ lib/functions/conversions.dart
@@ -1,4 +1,49 @@
 import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:pi_hole_client/models/domain.dart';
+import 'package:pi_hole_client/config/theme.dart';
+
+/// Converts a given [color] to the corresponding theme-based color defined in
+/// [AppColors].
+///
+/// This function is used to adapt predefined colors (such as [Colors.red],
+/// [Colors.green], etc.) to theme-specific variants from [AppColors]. If the
+/// provided color matches a predefined color, the corresponding color from
+/// [AppColors] is returned. If no match is found, the original color is
+/// returned unchanged.
+///
+/// This is particularly useful for applying light and dark theme adjustments
+/// to predefined colors while keeping the structure of [QueryStatus] unchanged.
+///
+///
+/// ### Parameters:
+/// - [colors]: An instance of [AppColors] that contains the theme-based color
+/// definitions.
+/// - [color]: The original color to be converted.
+///
+/// ### Returns:
+/// A theme-adjusted [Color] if a match is found in [AppColors], otherwise
+/// returns the original [color].
+Color convertColor(AppColors colors, Color color) {
+  switch (color) {
+    case Colors.red:
+      return colors.queryRed ?? Colors.red;
+    case Colors.green:
+      return colors.queryGreen ?? Colors.green;
+    case Colors.blue:
+      return colors.queryBlue ?? Colors.blue;
+    case Colors.orange:
+      return colors.queryOrange ?? Colors.orange;
+    case Colors.grey:
+      return colors.queryGrey ?? Colors.grey;
+    default:
+      return color;
+  }
+}
+
+List<Domain> parseDomainList(List<Map<String, dynamic>> jsonList) {
+  return jsonList.map((item) => Domain.fromJson(item)).toList();
+}
 
 bool? convertFromIntToBool(int value) {
   if (value == 1) {
