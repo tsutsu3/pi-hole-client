b31d709b1cde604ff3d873a1dcb3dee6a57647ac
tsutsu3 <tsutsu3prog@gmail.com>
2025-01-26 20:22:02 +0900

    Skip test

--- test/widgets/logs/logs_filters_modal_test.dart
+++ test/widgets/logs/logs_filters_modal_test.dart
@@ -1,51 +1,52 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:pi_hole_client/screens/logs/logs_filters_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pi_hole_client/screens/logs/logs_filters_modal.dart';
 
-// import '../helpers.dart';
+import '../helpers.dart';
 
-// TODO: couse
+// TODO: Fix this test
 // ═╡ EXCEPTION CAUGHT BY RENDERING LIBRARY ╞═════════════════════════════════════════════════════════
 // The following assertion was thrown during layout:
 
-// void main() async {
-//   await initializeApp();
+void main() async {
+  await initializeApp();
 
-//   group(
-//     'Query logs screen tests',
-//     () {
-//       late TestSetupHelper testSetup;
+  group(
+    'Query logs screen tests',
+    () {
+      late TestSetupHelper testSetup;
 
-//       setUp(() async {
-//         testSetup = TestSetupHelper();
-//         testSetup.initializeMock(useApiGatewayVersion: 'v6');
-//       });
+      setUp(() async {
+        testSetup = TestSetupHelper();
+        testSetup.initializeMock(useApiGatewayVersion: 'v6');
+      });
 
-//       testWidgets(
-//         'should show logs screen on mobile layout',
-//         (WidgetTester tester) async {
-//           tester.view.physicalSize = const Size(1080, 2400);
-//           tester.view.devicePixelRatio = 2.0;
+      testWidgets(
+        'should show logs screen on mobile layout',
+        (WidgetTester tester) async {
+          tester.view.physicalSize = const Size(1080, 2400);
+          tester.view.devicePixelRatio = 2.0;
 
-//           addTearDown(() {
-//             tester.view.resetPhysicalSize();
-//             tester.view.resetDevicePixelRatio();
-//           });
+          addTearDown(() {
+            tester.view.resetPhysicalSize();
+            tester.view.resetDevicePixelRatio();
+          });
 
-//           await tester.pumpWidget(
-//             testSetup.buildTestWidget(
-//               LogsFiltersModal(
-//                 statusBarHeight: 24.0,
-//                 bottomNavBarHeight: 48.0,
-//                 filterLogs: () {},
-//                 window: true,
-//               ),
-//             ),
-//           );
+          await tester.pumpWidget(
+            testSetup.buildTestWidget(
+              LogsFiltersModal(
+                statusBarHeight: 24.0,
+                bottomNavBarHeight: 48.0,
+                filterLogs: () {},
+                window: true,
+              ),
+            ),
+          );
 
-//           expect(find.byType(LogsFiltersModal), findsOneWidget);
-//         },
-//       );
-//     },
-//   );
-// }
+          expect(find.byType(LogsFiltersModal), findsOneWidget);
+        },
+        skip: true,
+      );
+    },
+  );
+}
