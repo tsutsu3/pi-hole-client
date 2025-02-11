2ce79fe72bdae2d428017b5878640a792df33926
tsutsu3 <tsutsu3prog@gmail.com>
2025-02-11 22:30:03 +0900

    Refactor Add Server Full Screen tests with TestSetupHelper

--- test/widgets/screens/servers/add_server_fullscreen_test.dart
+++ test/widgets/screens/servers/add_server_fullscreen_test.dart
@@ -1,76 +1,28 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:pi_hole_client/config/globals.dart';
-import 'package:pi_hole_client/config/theme.dart';
-import 'package:pi_hole_client/gateways/v5/api_gateway_v5.dart';
 import 'package:pi_hole_client/models/app_log.dart';
 import 'package:pi_hole_client/models/gateways.dart';
-import 'package:pi_hole_client/providers/app_config_provider.dart';
-// import 'package:pi_hole_client/models/server.dart';
-import 'package:pi_hole_client/providers/servers_provider.dart';
 import 'package:pi_hole_client/screens/servers/add_server_fullscreen.dart';
-import 'package:provider/provider.dart';
 
-import 'add_server_fullscreen_test.mocks.dart';
+import '../../helpers.dart';
 
-@GenerateMocks(
-  [AppConfigProvider, ServersProvider, ApiGatewayV5],
-)
 void main() async {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  await initializeApp();
 
   group('Add Server Full Screen Widget Tests', () {
-    late MockAppConfigProvider mockAppConfigProvider;
-    late MockServersProvider mockServersProvider;
-    late MockApiGatewayV5 mockApiGatewayV5;
-
-    // final server = Server(
-    //   address: 'http://localhost',
-    //   alias: 'v5',
-    //   defaultServer: false,
-    //   apiVersion: 'v5',
-    // );
+    late TestSetupHelper testSetup;
 
     setUp(() async {
-      FlutterSecureStorage.setMockInitialValues({});
-
-      mockAppConfigProvider = MockAppConfigProvider();
-      mockServersProvider = MockServersProvider();
-      mockApiGatewayV5 = MockApiGatewayV5();
-
-      when(mockApiGatewayV5.loginQuery()).thenAnswer(
-        (_) async => LoginQueryResponse(
-          result: APiResponseType.success,
-          status: 'enabled',
-          sid: 'sid123',
-        ),
-      );
-
-      const showingSnackbar = false;
-      when(mockAppConfigProvider.showingSnackbar).thenReturn(showingSnackbar);
-      when(mockAppConfigProvider.setShowingSnackbar(any)).thenAnswer((_) {});
-
-      when(mockServersProvider.checkUrlExists(any))
-          .thenAnswer((_) async => {'result': 'success', 'exists': false});
-      when(mockServersProvider.addServer(any)).thenAnswer((_) async => true);
-      when(mockServersProvider.editServer(any)).thenAnswer((_) async => true);
-      when(mockServersProvider.loadApiGateway(any))
-          .thenReturn(mockApiGatewayV5);
+      testSetup = TestSetupHelper();
+      testSetup.initializeMock(useApiGatewayVersion: 'v6');
     });
 
     testWidgets(
       'should show the successful snackbar when adding a server',
       (WidgetTester tester) async {
-        tester.view.physicalSize = const Size(600, 1000);
-        tester.view.devicePixelRatio = 1.0;
+        tester.view.physicalSize = const Size(1080, 2400);
+        tester.view.devicePixelRatio = 2.0;
 
         addTearDown(() {
           tester.view.resetPhysicalSize();
@@ -78,48 +30,18 @@ void main() async {
         });
 
         await tester.pumpWidget(
-          DynamicColorBuilder(
-            builder: (lightDynamic, darkDynamic) {
-              return MultiProvider(
-                providers: [
-                  ChangeNotifierProvider<ServersProvider>(
-                    create: (context) => mockServersProvider,
-                  ),
-                  ChangeNotifierProvider<AppConfigProvider>(
-                    create: (context) => mockAppConfigProvider,
-                  ),
-                  ChangeNotifierProxyProvider<AppConfigProvider,
-                      ServersProvider>(
-                    create: (context) => mockServersProvider,
-                    update: (context, appConfig, servers) =>
-                        servers!..update(appConfig),
-                  ),
-                ],
-                child: MaterialApp(
-                  theme: lightTheme(lightDynamic),
-                  darkTheme: darkTheme(darkDynamic),
-                  themeMode: ThemeMode.light,
-                  home: const Scaffold(
-                    body: AddServerFullscreen(
-                      window: false,
-                      title: 'test',
-                    ),
-                  ),
-                  localizationsDelegates: const [
-                    GlobalMaterialLocalizations.delegate,
-                    AppLocalizations.delegate,
-                  ],
-                  scaffoldMessengerKey: scaffoldMessengerKey,
-                ),
-              );
-            },
+          testSetup.buildTestWidget(
+            const AddServerFullscreen(
+              window: false,
+              title: 'test',
+            ),
           ),
         );
 
         expect(find.byType(AddServerFullscreen), findsOneWidget);
         expect(find.byIcon(Icons.login_rounded), findsOneWidget);
 
-        await tester.enterText(find.byType(TextField).at(0), 'v5'); // Alias
+        await tester.enterText(find.byType(TextField).at(0), 'v6'); // Alias
         await tester.enterText(
           find.byType(TextField).at(1),
           'localhost',
@@ -139,11 +61,12 @@ void main() async {
     testWidgets(
       'should show the error snackbar when adding a server',
       (WidgetTester tester) async {
-        tester.view.physicalSize = const Size(600, 1000);
-        tester.view.devicePixelRatio = 1.0;
+        tester.view.physicalSize = const Size(1080, 2400);
+        tester.view.devicePixelRatio = 2.0;
 
-        when(mockServersProvider.addServer(any)).thenAnswer((_) async => false);
-        when(mockApiGatewayV5.loginQuery()).thenAnswer(
+        when(testSetup.mockServersProvider.addServer(any))
+            .thenAnswer((_) async => false);
+        when(testSetup.mockApiGatewayV6.loginQuery()).thenAnswer(
           (_) async => LoginQueryResponse(
             result: APiResponseType.socket,
             log: AppLog(
@@ -160,48 +83,18 @@ void main() async {
         });
 
         await tester.pumpWidget(
-          DynamicColorBuilder(
-            builder: (lightDynamic, darkDynamic) {
-              return MultiProvider(
-                providers: [
-                  ChangeNotifierProvider<ServersProvider>(
-                    create: (context) => mockServersProvider,
-                  ),
-                  ChangeNotifierProvider<AppConfigProvider>(
-                    create: (context) => mockAppConfigProvider,
-                  ),
-                  ChangeNotifierProxyProvider<AppConfigProvider,
-                      ServersProvider>(
-                    create: (context) => mockServersProvider,
-                    update: (context, appConfig, servers) =>
-                        servers!..update(appConfig),
-                  ),
-                ],
-                child: MaterialApp(
-                  theme: lightTheme(lightDynamic),
-                  darkTheme: darkTheme(darkDynamic),
-                  themeMode: ThemeMode.light,
-                  home: const Scaffold(
-                    body: AddServerFullscreen(
-                      window: false,
-                      title: 'test',
-                    ),
-                  ),
-                  localizationsDelegates: const [
-                    GlobalMaterialLocalizations.delegate,
-                    AppLocalizations.delegate,
-                  ],
-                  scaffoldMessengerKey: scaffoldMessengerKey,
-                ),
-              );
-            },
+          testSetup.buildTestWidget(
+            const AddServerFullscreen(
+              window: false,
+              title: 'test',
+            ),
           ),
         );
 
         expect(find.byType(AddServerFullscreen), findsOneWidget);
         expect(find.byIcon(Icons.login_rounded), findsOneWidget);
 
-        await tester.enterText(find.byType(TextField).at(0), 'v5'); // Alias
+        await tester.enterText(find.byType(TextField).at(0), 'v6'); // Alias
         await tester.enterText(
           find.byType(TextField).at(1),
           'localhost',
