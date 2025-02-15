1912a0898c6a5ac2b8de27a45c4d03bd33b1be05
tsutsu3 <tsutsu3prog@gmail.com>
2025-02-15 22:12:54 +0900

    Add tests

--- test/widgets/screens/servers/add_server_fullscreen_test.dart
+++ test/widgets/screens/servers/add_server_fullscreen_test.dart
@@ -100,6 +100,14 @@ void main() async {
           'localhost',
         ); // IP Address
         await tester.enterText(
+          find.byType(TextField).at(1),
+          '/test',
+        ); // subroute
+        await tester.enterText(
+          find.byType(TextField).at(1),
+          '8080',
+        ); // port
+        await tester.enterText(
           find.byType(TextField).at(4),
           'test123',
         ); // token
@@ -108,6 +116,58 @@ void main() async {
         await tester.pump(const Duration(milliseconds: 1000));
         expect(find.byType(SnackBar), findsOneWidget);
         expect(find.text('Failed. Check address.'), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'should show validation error when adding a server',
+      (WidgetTester tester) async {
+        tester.view.physicalSize = const Size(1080, 2400);
+        tester.view.devicePixelRatio = 2.0;
+
+        addTearDown(() {
+          tester.view.resetPhysicalSize();
+          tester.view.resetDevicePixelRatio();
+        });
+
+        await tester.pumpWidget(
+          testSetup.buildTestWidget(
+            const AddServerFullscreen(
+              window: false,
+              title: 'test',
+            ),
+          ),
+        );
+
+        expect(find.byType(AddServerFullscreen), findsOneWidget);
+        expect(find.byIcon(Icons.login_rounded), findsOneWidget);
+
+        await tester.enterText(
+          find.byType(TextField).at(1),
+          '@',
+        ); // IP Address
+
+        await tester.enterText(
+          find.byType(TextField).at(2),
+          '@',
+        ); // subroute
+
+        await tester.enterText(
+          find.byType(TextField).at(3),
+          '@',
+        ); // port
+
+        await tester.pump();
+        await tester.pump();
+        await tester.pump();
+
+        expect(find.text('Invalid IP or domain'), findsOneWidget);
+        expect(
+          find.text(
+              "Invalid subroute. Remember not to finish with a '/', '.' or ':'."),
+          findsOneWidget,
+        );
+        expect(find.text('Invalid port'), findsOneWidget);
       },
     );
 
