eb72575c7cfd334a441d0f90e8c5e0670a9f4e83
tsutsu3 <tsutsu3prog@gmail.com>
2025-02-24 20:37:32 +0900

    Update comment edit

--- lib/screens/domains/domain_comment_modal.dart
+++ lib/screens/domains/domain_comment_modal.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:pi_hole_client/l10n/generated/app_localizations.dart';
+import 'package:pi_hole_client/models/domain.dart';
 
-class DomainCommentModal extends StatelessWidget {
-  const DomainCommentModal({required this.comment, super.key});
+class DomainCommentModal extends StatefulWidget {
+  const DomainCommentModal({
+    required this.domain,
+    required this.readonly,
+    super.key,
+  });
 
-  final String comment;
+  final Domain domain;
+  final bool readonly;
+
+  @override
+  State<DomainCommentModal> createState() => _DomainCommentModalState();
+}
+
+class _DomainCommentModalState extends State<DomainCommentModal> {
+  late TextEditingController _commentController;
+  late bool readonly = widget.readonly;
+
+  bool isSaveEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _commentController = TextEditingController(text: widget.domain.comment);
+    _commentController.addListener(() {
+      setState(() {
+        isSaveEnabled = _commentController.text.isNotEmpty;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _commentController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +59,44 @@ class DomainCommentModal extends StatelessWidget {
           ),
         ],
       ),
-      content: Text(
-        comment,
-      ),
+      content: readonly
+          // v5: read only, because the API does not support updating comments
+          ? Text(
+              widget.domain.comment == ''
+                  ? AppLocalizations.of(context)!.noComment
+                  : widget.domain.comment!,
+              style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+            )
+          // v6: editable
+          : TextField(
+              controller: _commentController,
+              onChanged: (value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                hintText: AppLocalizations.of(context)!.enterComment,
+              ),
+            ),
       actions: [
         TextButton(
-          onPressed: () => Navigator.maybePop(context),
-          child: Text(AppLocalizations.of(context)!.close),
+          onPressed: () => Navigator.maybePop(context, widget.domain.comment),
+          child: Text(AppLocalizations.of(context)!.cancel),
         ),
+        const SizedBox(width: 14),
+        if (!readonly)
+          TextButton(
+            onPressed: isSaveEnabled
+                ? () async {
+                    await Navigator.maybePop(context, _commentController.text);
+                  }
+                : null,
+            style: ButtonStyle(
+              foregroundColor: WidgetStateProperty.all(
+                isSaveEnabled ? null : Colors.grey,
+              ),
+            ),
+            child: Text(AppLocalizations.of(context)!.save),
+          ),
       ],
     );
   }
--- lib/screens/domains/domain_details_screen.dart
+++ lib/screens/domains/domain_details_screen.dart
@@ -5,9 +5,13 @@ import 'package:pi_hole_client/functions/conversions.dart';
 import 'package:pi_hole_client/functions/format.dart';
 import 'package:pi_hole_client/l10n/generated/app_localizations.dart';
 import 'package:pi_hole_client/models/domain.dart';
+import 'package:pi_hole_client/models/gateways.dart';
+import 'package:pi_hole_client/providers/domains_list_provider.dart';
+import 'package:pi_hole_client/providers/servers_provider.dart';
 import 'package:pi_hole_client/screens/domains/delete_domain_modal.dart';
 import 'package:pi_hole_client/screens/domains/domain_comment_modal.dart';
 import 'package:pi_hole_client/widgets/custom_list_tile.dart';
+import 'package:provider/provider.dart';
 
 class DomainDetailsScreen extends StatelessWidget {
   const DomainDetailsScreen({
@@ -17,12 +21,69 @@ class DomainDetailsScreen extends StatelessWidget {
     this.colors,
   });
 
-  final Domain domain;
+  final Domain domain; // useonly id
   final void Function(Domain) remove;
   final AppColors? colors;
 
+  Widget actionCustomListTile({
+    required IconData leadingIcon,
+    required String label,
+    required String? description,
+    required GestureTapCallback? onTap,
+    Color? color,
+    Widget? trailing,
+  }) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        child: CustomListTile(
+          leadingIcon: leadingIcon,
+          label: label,
+          description: description,
+          color: color,
+          trailing: trailing,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final domainProvider = Provider.of<DomainsListProvider>(context);
+    final serverProvider = Provider.of<ServersProvider>(context, listen: false);
+    final apiGateway = serverProvider.selectedApiGateway;
+    var newDomain = domainProvider.getDomainById(domain.id);
+
+    if (newDomain == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.domainDetails),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.error,
+                size: 50,
+                color: Colors.red,
+              ),
+              const SizedBox(height: 50),
+              Text(
+                AppLocalizations.of(context)!.domainNotExists,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  fontSize: 22,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.domainDetails),
@@ -48,52 +109,68 @@ class DomainDetailsScreen extends StatelessWidget {
             CustomListTile(
               leadingIcon: Icons.domain,
               label: AppLocalizations.of(context)!.domain,
-              description: domain.domain,
+              description: newDomain.domain,
             ),
             CustomListTile(
               leadingIcon: Icons.category_rounded,
               label: AppLocalizations.of(context)!.type,
-              description: getDomainType(domain.type),
+              description: getDomainType(newDomain.type),
               color: colors != null
-                  ? convertColorFromNumber(colors!, domain.type)
+                  ? convertColorFromNumber(colors!, newDomain.type)
                   : null,
             ),
             CustomListTile(
               leadingIcon: Icons.schedule_rounded,
               label: AppLocalizations.of(context)!.dateAdded,
-              description: formatTimestamp(domain.dateAdded, 'yyyy-MM-dd'),
+              description: formatTimestamp(newDomain.dateAdded, 'yyyy-MM-dd'),
             ),
             CustomListTile(
               leadingIcon: Icons.update_rounded,
               label: AppLocalizations.of(context)!.dateModified,
-              description: formatTimestamp(domain.dateModified, 'yyyy-MM-dd'),
+              description:
+                  formatTimestamp(newDomain.dateModified, 'yyyy-MM-dd'),
             ),
             CustomListTile(
               leadingIcon: Icons.check,
               label: AppLocalizations.of(context)!.status,
-              description: domain.enabled == 1
+              description: newDomain.enabled == 1
                   ? AppLocalizations.of(context)!.enabled
                   : AppLocalizations.of(context)!.disabled,
             ),
-            Material(
-              color: Colors.transparent,
-              child: InkWell(
-                onTap: domain.comment != null && domain.comment != ''
-                    ? () => {
-                          showModal(
-                            context: context,
-                            builder: (context) =>
-                                DomainCommentModal(comment: domain.comment!),
-                          ),
-                        }
-                    : null,
-                child: CustomListTile(
-                  leadingIcon: Icons.comment_rounded,
-                  label: AppLocalizations.of(context)!.comment,
-                  description: domain.comment == ''
-                      ? AppLocalizations.of(context)!.noComment
-                      : domain.comment,
-                ),
+            // Comment
+            actionCustomListTile(
+              leadingIcon: Icons.comment_rounded,
+              label: AppLocalizations.of(context)!.comment,
+              description: newDomain.comment == '' || newDomain.comment == null
+                  ? AppLocalizations.of(context)!.noComment
+                  : newDomain.comment,
+              onTap: () async {
+                final newComment = await showModal<String>(
+                  context: context,
+                  builder: (context) => DomainCommentModal(
+                    domain: newDomain!,
+                    readonly:
+                        apiGateway!.server.apiVersion == 'v5' ? true : false,
+                  ),
+                );
+
+                if (newComment != null && newComment != newDomain!.comment) {
+                  if (context.mounted) {
+                    newDomain = newDomain!.copyWith(comment: newComment);
+                    final resp = await apiGateway!.updateDomain(newDomain!);
+                    if (resp.result == APiResponseType.success) {
+                      domainProvider.updateDomain(newDomain!);
+                    }
+                  }
+                }
+              },
+              trailing: Icon(
+                Icons.open_in_new_rounded,
+                size: 20,
+                color: Theme.of(context)
+                    .colorScheme
+                    .onSurfaceVariant
+                    .withValues(alpha: 0.6),
               ),
             ),
           ],
