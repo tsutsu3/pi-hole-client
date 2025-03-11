aee1712590827d4e9cd9fed87ddf2010eea87564
tsutsu3 <tsutsu3prog@gmail.com>
2025-03-12 01:04:07 +0900

    Add unit tests for color manipulation methods

--- test/ut/config/theme_test.dart
+++ test/ut/config/theme_test.dart
@@ -85,4 +85,223 @@ void main() {
       expect(graphColors!.colors.length, lightGraphColors.colors.length);
     });
   });
+
+  test('DataVisColors copyWith should return new instance with updated values',
+      () {
+    const original = DataVisColors(
+      blue: Colors.blue,
+      blueDark: Colors.blueGrey,
+      green: Colors.green,
+      greenDark: Colors.greenAccent,
+      orange: Colors.orange,
+      orangeDark: Colors.deepOrange,
+      red: Colors.red,
+      redDark: Colors.redAccent,
+    );
+
+    final modified = original.copyWith(blue: Colors.lightBlue);
+
+    expect(modified.blue, Colors.lightBlue);
+    expect(modified.blueDark, original.blueDark);
+  });
+
+  test('AppColors copyWith should return new instance with updated values', () {
+    const original = AppColors(
+      snackBarSuccess: Colors.green,
+      snackBarSuccessText: Colors.white,
+      snackBarCaution: Colors.amber,
+      snackBarCautionText: Colors.black,
+      snackBarError: Colors.red,
+      snackBarErrorText: Colors.white,
+      snackBarNeutral: Colors.blueGrey,
+      snackBarNeutralText: Colors.black,
+      cardWarning: Colors.yellow,
+      cardWarningText: Colors.black,
+      queryRed: Colors.red,
+      queryGreen: Colors.green,
+      queryBlue: Colors.blue,
+      queryOrange: Colors.orange,
+      queryGrey: Colors.grey,
+      commonRed: Colors.redAccent,
+      commonGreen: Colors.greenAccent,
+    );
+
+    final modified = original.copyWith(snackBarSuccess: Colors.lightGreen);
+
+    expect(modified.snackBarSuccess, Colors.lightGreen);
+    expect(modified.snackBarError, original.snackBarError);
+  });
+
+  test('DataVisColors lerp should interpolate colors correctly', () {
+    const start = DataVisColors(
+      blue: Colors.blue,
+      blueDark: Colors.blueGrey,
+      green: Colors.green,
+      greenDark: Colors.greenAccent,
+      orange: Colors.orange,
+      orangeDark: Colors.deepOrange,
+      red: Colors.red,
+      redDark: Colors.redAccent,
+    );
+
+    const end = DataVisColors(
+      blue: Colors.lightBlue,
+      blueDark: Colors.black,
+      green: Colors.lightGreen,
+      greenDark: Colors.black,
+      orange: Colors.orangeAccent,
+      orangeDark: Colors.black,
+      red: Colors.pink,
+      redDark: Colors.black,
+    );
+
+    final lerped = start.lerp(end, 0.5);
+
+    expect(lerped.blue, Color.lerp(Colors.blue, Colors.lightBlue, 0.5));
+    expect(lerped.red, Color.lerp(Colors.red, Colors.pink, 0.5));
+  });
+
+  test('AppColors lerp should interpolate colors correctly', () {
+    const start = AppColors(
+      snackBarSuccess: Colors.green,
+      snackBarSuccessText: Colors.white,
+      snackBarCaution: Colors.amber,
+      snackBarCautionText: Colors.black,
+      snackBarError: Colors.red,
+      snackBarErrorText: Colors.white,
+      snackBarNeutral: Colors.blueGrey,
+      snackBarNeutralText: Colors.black,
+      cardWarning: Colors.yellow,
+      cardWarningText: Colors.black,
+      queryRed: Colors.red,
+      queryGreen: Colors.green,
+      queryBlue: Colors.blue,
+      queryOrange: Colors.orange,
+      queryGrey: Colors.grey,
+      commonRed: Colors.redAccent,
+      commonGreen: Colors.greenAccent,
+    );
+
+    const end = AppColors(
+      snackBarSuccess: Colors.lightGreen,
+      snackBarSuccessText: Colors.black,
+      snackBarCaution: Colors.orange,
+      snackBarCautionText: Colors.white,
+      snackBarError: Colors.deepOrange,
+      snackBarErrorText: Colors.black,
+      snackBarNeutral: Colors.black,
+      snackBarNeutralText: Colors.white,
+      cardWarning: Colors.deepOrange,
+      cardWarningText: Colors.white,
+      queryRed: Colors.pink,
+      queryGreen: Colors.teal,
+      queryBlue: Colors.cyan,
+      queryOrange: Colors.deepOrange,
+      queryGrey: Colors.white,
+      commonRed: Colors.black,
+      commonGreen: Colors.white,
+    );
+
+    final lerped = start.lerp(end, 0.5);
+
+    expect(
+      lerped.snackBarSuccess,
+      Color.lerp(Colors.green, Colors.lightGreen, 0.5),
+    );
+    expect(lerped.queryRed, Color.lerp(Colors.red, Colors.pink, 0.5));
+  });
+
+  test('GraphColors lerp should interpolate colors correctly', () {
+    const start = GraphColors(
+      colors: [
+        Colors.red,
+        Colors.green,
+        Colors.blue,
+      ],
+    );
+
+    const end = GraphColors(
+      colors: [
+        Colors.pink,
+        Colors.teal,
+        Colors.cyan,
+      ],
+    );
+
+    final lerped = start.lerp(end, 0.5);
+
+    expect(lerped.colors.length, start.colors.length);
+    expect(lerped.colors[0], Color.lerp(Colors.red, Colors.pink, 0.5));
+    expect(lerped.colors[1], Color.lerp(Colors.green, Colors.teal, 0.5));
+    expect(lerped.colors[2], Color.lerp(Colors.blue, Colors.cyan, 0.5));
+  });
+
+  test('DataVisColors harmonized should modify colors based on ColorScheme',
+      () {
+    const original = DataVisColors(
+      blue: Colors.blue,
+      blueDark: Colors.blueGrey,
+      green: Colors.green,
+      greenDark: Colors.greenAccent,
+      orange: Colors.orange,
+      orangeDark: Colors.deepOrange,
+      red: Colors.red,
+      redDark: Colors.redAccent,
+    );
+
+    final colorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+
+    final harmonized = original.harmonized(colorScheme);
+
+    expect(harmonized.blue, isNot(equals(original.blue)));
+    expect(harmonized.red, isNot(equals(original.red)));
+  });
+
+  test('AppColors harmonized should modify colors based on ColorScheme', () {
+    const original = AppColors(
+      snackBarSuccess: Colors.green,
+      snackBarSuccessText: Colors.white,
+      snackBarCaution: Colors.amber,
+      snackBarCautionText: Colors.black,
+      snackBarError: Colors.red,
+      snackBarErrorText: Colors.white,
+      snackBarNeutral: Colors.blueGrey,
+      snackBarNeutralText: Colors.black,
+      cardWarning: Colors.yellow,
+      cardWarningText: Colors.black,
+      queryRed: Colors.red,
+      queryGreen: Colors.green,
+      queryBlue: Colors.blue,
+      queryOrange: Colors.orange,
+      queryGrey: Colors.grey,
+      commonRed: Colors.redAccent,
+      commonGreen: Colors.greenAccent,
+    );
+
+    final colorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+
+    final harmonized = original.harmonized(colorScheme);
+
+    expect(harmonized.snackBarSuccess, isNot(equals(original.snackBarSuccess)));
+    expect(harmonized.queryRed, isNot(equals(original.queryRed)));
+  });
+
+  test('GraphColors harmonized should modify colors based on ColorScheme', () {
+    const original = GraphColors(
+      colors: [
+        Colors.red,
+        Colors.green,
+        Colors.blue,
+      ],
+    );
+
+    final colorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+
+    final harmonized = original.harmonized(colorScheme);
+
+    expect(harmonized.colors.length, original.colors.length);
+    expect(harmonized.colors[0], isNot(equals(original.colors[0])));
+    expect(harmonized.colors[1], isNot(equals(original.colors[1])));
+    expect(harmonized.colors[2], isNot(equals(original.colors[2])));
+  });
 }
