bcae070f75b17729c88c0f79d611ef82a3a7d626
tsutsu3 <tsutsu3prog@gmail.com>
2025-05-08 20:44:20 +0900

    Remove unused import from chart visualization screen test

--- test/widgets/screens/settings/app_settings/advanced_settings/chart_visualization_screen_test.dart
+++ test/widgets/screens/settings/app_settings/advanced_settings/chart_visualization_screen_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pi_hole_client/screens/settings/app_settings/advanced_settings/advanced_options.dart';
 import 'package:pi_hole_client/screens/settings/app_settings/advanced_settings/chart_visualization_screen.dart';
 
 import '../../../../helpers.dart';
