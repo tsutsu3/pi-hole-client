59cdd60b9735776f2f59cc0e0a7d1f1a591d81b6
tsutsu3 <tsutsu3prog@gmail.com>
2025-06-01 20:04:52 +0900

    Refactor DeleteModal

--- lib/widgets/delete_modal.dart
+++ lib/widgets/delete_modal.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pi_hole_client/l10n/generated/app_localizations.dart';
+import 'package:pi_hole_client/widgets/confirmation_modal.dart';
 
 class DeleteModal extends StatelessWidget {
   const DeleteModal({
@@ -17,75 +18,15 @@ class DeleteModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Column(
-        children: [
-          Icon(
-            Icons.delete_rounded,
-            size: 24,
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            title,
-            style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
-          ),
-        ],
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            message,
-            style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
-          ),
-          if (submessage != null) ...[
-            const SizedBox(height: 8),
-            Text(
-              submessage!,
-              style: const TextStyle(fontStyle: FontStyle.italic),
-            ),
-          ],
-        ],
-      ),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              onPressed: () => Navigator.maybePop(context),
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-              ),
-              child: Text(
-                AppLocalizations.of(context)!.cancel,
-                style: TextStyle(
-                  color: Theme.of(context)
-                      .colorScheme
-                      .onSurface
-                      .withValues(alpha: 0.8),
-                ),
-              ),
-            ),
-            const SizedBox(width: 16),
-            ElevatedButton(
-              onPressed: onDelete,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.redAccent,
-                elevation: 0,
-                shadowColor: Colors.transparent,
-              ),
-              child: Text(
-                AppLocalizations.of(context)!.delete,
-                style: TextStyle(
-                  color: Colors.white.withValues(alpha: 0.9),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ],
+    return ConfirmationModal(
+      title: title,
+      message: message,
+      submessage: submessage,
+      onConfirm: onDelete,
+      confirmButtonText: AppLocalizations.of(context)!.delete,
+      icon: Icons.delete_rounded,
+      confirmButtonColor: Colors.redAccent,
+      confirmButtonTextColor: Colors.white,
     );
   }
 }
