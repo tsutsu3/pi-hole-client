5bf3017cf4c0abea886349e26584fe9d20e1f24f
tsutsu3 <tsutsu3prog@gmail.com>
2025-06-09 21:12:56 +0900

    Add session mock api endpoints

--- mock_api_server/lib/handlers/auth_handler.dart
+++ mock_api_server/lib/handlers/auth_handler.dart
@@ -48,6 +48,66 @@ class AuthHandler {
       return Response(204);
     });
 
+    router.get('/sessions', (Request request) {
+      final mockResponse = {
+        'sessions': [
+          {
+            'id': 0,
+            'current_session': false,
+            'valid': false,
+            'tls': {'login': false, 'mixed': false},
+            'app': false,
+            'cli': false,
+            'login_at': 1580000000,
+            'last_active': 1580000000,
+            'valid_until': 1580000300,
+            'remote_addr': '192.168.0.30',
+            'user_agent': 'Dart/3.7 (dart:io)',
+            'x_forwarded_for': null,
+          },
+          {
+            'id': 1,
+            'current_session': true,
+            'valid': true,
+            'tls': {'login': true, 'mixed': false},
+            'app': false,
+            'cli': false,
+            'login_at': 1580000000,
+            'last_active': 1580000000,
+            'valid_until': 1580000300,
+            'remote_addr': '192.168.0.31',
+            'user_agent':
+                'Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0',
+            'x_forwarded_for': null,
+          },
+          {
+            'id': 3,
+            'current_session': false,
+            'valid': false,
+            'tls': {'login': false, 'mixed': true},
+            'app': true,
+            'cli': true,
+            'login_at': 1580000000,
+            'last_active': 1580000000,
+            'valid_until': 1580000300,
+            'remote_addr': '192.168.0.32',
+            'user_agent': 'Dart/3.7 (dart:io)',
+            'x_forwarded_for': null,
+          },
+        ],
+        'took': 0.003,
+      };
+
+      return Response.ok(
+        jsonEncode(mockResponse),
+        headers: {'Content-Type': 'application/json'},
+      );
+    });
+
+    router.delete('/session/<id>', (Request request, String id) {
+      return Response(204);
+    });
+
     return router;
   }
 }
