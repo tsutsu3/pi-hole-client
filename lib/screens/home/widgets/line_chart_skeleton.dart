579e6967cafd5c65d241bb187b26b54daa9382aa
tsutsu3 <tsutsu3prog@gmail.com>
2025-06-30 22:07:31 +0900

    import loading_animation_widget at the top of line_chart_skeleton.dart

--- lib/screens/home/widgets/line_chart_skeleton.dart
+++ lib/screens/home/widgets/line_chart_skeleton.dart
@@ -2,10 +2,10 @@ import 'dart:math';
 
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:pi_hole_client/config/theme.dart';
 import 'package:pi_hole_client/l10n/generated/app_localizations.dart';
 import 'package:skeletonizer/skeletonizer.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LineChartSkeleton extends StatefulWidget {
   const LineChartSkeleton({
