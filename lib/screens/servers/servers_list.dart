11cc9405fc34c48babeccfc4aa2b47742be9d2cd
tsutsu3 <tsutsu3prog@gmail.com>
2025-07-06 09:21:05 +0900

    fix(servers): use watch instead of select to show server tile immediately after adding

--- lib/screens/servers/servers_list.dart
+++ lib/screens/servers/servers_list.dart
@@ -1,7 +1,6 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:pi_hole_client/l10n/generated/app_localizations.dart';
-import 'package:pi_hole_client/models/server.dart';
 import 'package:pi_hole_client/providers/servers_provider.dart';
 import 'package:pi_hole_client/screens/servers/servers_tile_item.dart';
 import 'package:provider/provider.dart';
@@ -24,9 +23,7 @@ class ServersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final serversList = context.select<ServersProvider, List<Server>>(
-      (serversProvider) => serversProvider.getServersList,
-    );
+    final serversList = context.watch<ServersProvider>().getServersList;
 
     if (serversList.isNotEmpty) {
       return ListView(
