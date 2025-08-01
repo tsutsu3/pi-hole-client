a872558793094915499465c10cd83f90e3d6284a
tsutsu3 <tsutsu3prog@gmail.com>
2025-08-02 01:11:05 +0900

    Implement AuthRepositoryV5 with not supported feature handling

--- lib/data/repositories/api/v5/auth_repository.dart
+++ lib/data/repositories/api/v5/auth_repository.dart
@@ -1,1 +1,50 @@
-abstract interface class AuthRepository {}
+import 'package:http/http.dart' as http;
+import 'package:pi_hole_client/data/repositories/api/interfaces/auth_repository.dart';
+import 'package:pi_hole_client/data/repositories/utils/constants.dart';
+import 'package:pi_hole_client/data/repositories/utils/exceptions.dart';
+import 'package:pi_hole_client/domain/models/auth.dart';
+import 'package:pi_hole_client/domain/models/auth_session.dart';
+import 'package:result_dart/result_dart.dart';
+
+class AuthRepositoryV5 implements AuthRepository {
+  AuthRepositoryV5({
+    required String sid,
+    required String url,
+    http.Client? client,
+    bool? allowSelfSignedCert,
+  }) {
+    // "Use" the parameters to suppress Lint
+    sid.hashCode;
+    url.hashCode;
+    client.hashCode;
+    allowSelfSignedCert.hashCode;
+  }
+
+  @override
+  Future<Result<Auth>> createSession(String password) {
+    return Future.value(
+      Failure(NotSupportedException(kNotSupportedInV5Message)),
+    );
+  }
+
+  @override
+  Future<Result<void>> deleteCurrentSession() async {
+    return Future.value(
+      Failure(NotSupportedException(kNotSupportedInV5Message)),
+    );
+  }
+
+  @override
+  Future<Result<List<AuthSession>>> getAllSessions() async {
+    return Future.value(
+      Failure(NotSupportedException(kNotSupportedInV5Message)),
+    );
+  }
+
+  @override
+  Future<Result<void>> deleteSessionById(int id) async {
+    return Future.value(
+      Failure(NotSupportedException(kNotSupportedInV5Message)),
+    );
+  }
+}
---
+++ lib/data/repositories/utils/constants.dart
@@ -0,0 +1,1 @@
+const kNotSupportedInV5Message = 'Pi-hole v5 does not support this feature.';
