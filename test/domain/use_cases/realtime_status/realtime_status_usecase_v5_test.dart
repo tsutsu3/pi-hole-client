1b2302fded416f538a4425a194879ca4ddf20b6c
tsutsu3 <tsutsu3prog@gmail.com>
2025-08-16 20:19:47 +0900

    Add v5 realtime usecate tests

---
+++ test/domain/use_cases/realtime_status/realtime_status_usecase_v5_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pi_hole_client/domain/use_cases/realtime_status/realtime_status_usecase_v5.dart';
+
+import '../../../../testing/fakes/models/v5/realtime_status.dart';
+import '../../../../testing/fakes/repositories/api/v5/fake_realtime_status_repository.dart';
+import '../../../../testing/helper/test_helper.dart';
+
+void main() {
+  group('RealTimeStatusUseCaseV5', () {
+    late FakeRealTimeStatusRepositoryV5 repository;
+    late RealtimeStatusUseCaseV5 useCase;
+
+    setUp(() {
+      repository = FakeRealTimeStatusRepositoryV5();
+      useCase = RealtimeStatusUseCaseV5(repository: repository);
+    });
+
+    test('should fetch real-time status successfully', () async {
+      final result = await useCase.fetchRealtimeStatus();
+      expect(result.isSuccess(), true);
+      expect(result.getOrNull(), kRepoFetchRealTimeStatus);
+    });
+
+    test('should handle errors when fetching real-time status', () async {
+      repository.shouldFail = true;
+      final result = await useCase.fetchRealtimeStatus();
+      expect(result.isError(), true);
+      expectError(result, messageContains: 'Failed to fetch real-time status');
+    });
+  });
+}
---
+++ testing/fakes/repositories/api/v5/fake_realtime_status_repository.dart
@@ -0,0 +1,17 @@
+import 'package:pi_hole_client/data/repositories/api/v5/realtime_status_repository.dart';
+import 'package:pi_hole_client/domain/model/realtime_status/realtime_status.dart';
+import 'package:result_dart/result_dart.dart';
+
+import '../../../models/v5/realtime_status.dart';
+import 'fake_base_v5_token_repository.dart';
+
+class FakeRealTimeStatusRepositoryV5 extends FakeBaseV5TokenRepository
+    implements RealTimeStatusRepositoryV5 {
+  @override
+  Future<Result<RealtimeStatus>> fetchRealtimeStatus() async {
+    if (shouldFail) {
+      return Failure(Exception('Failed to fetch real-time status'));
+    }
+    return Success(kRepoFetchRealTimeStatus);
+  }
+}
