cbcf0091d781e5c1a0e2735012f52933a95ac937
tsutsu3 <tsutsu3prog@gmail.com>
2026-01-11 17:03:47 +0900

    feat(website): implement ImageRow component and update documentation to use it

---
+++ website/src/components/image-row.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ImageRow({children}) {
+  return (
+    <div style={{ display: 'flex', gap: '12px', flexWrap: 'wrap', marginBottom: '12px' }}>
+      {children}
+    </div>
+  );
+}
