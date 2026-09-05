a63c81d1bd5fb475fce94da6beebe21e6db18312
tsutsu3 <tsutsu3prog@gmail.com>
2026-09-06 06:07:03 +0900

    ci(website): migrate site deployment to Cloudflare (#702)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -9,7 +9,7 @@ import starlightLinksValidator from "starlight-links-validator";
 import { meta } from "./src/i18n/meta";
 
 const githubUrl = "https://github.com/tsutsu3/pi-hole-client";
-const websiteUrl = "https://tsutsu3.github.io/pi-hole-client";
+const websiteUrl = "https://pi-hole-client.tsutsu3.com";
 const ogImageUrl = `${websiteUrl}/img/feature-image-og.png`;
 const description = meta.description.en;
 
@@ -191,8 +191,8 @@ const sidebar = [
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tsutsu3.github.io",
-  base: "/pi-hole-client",
+  site: "https://pi-hole-client.tsutsu3.com",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     react(),
